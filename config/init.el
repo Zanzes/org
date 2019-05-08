5b2f6a8a29dd1443f69aa08873f17bb036184609
Laz <nikolajea@gmail.com>
2019-05-08 19:12:35 +0200

    Newest

--- config/init.el
+++ config/init.el
@@ -1,19 +1,25 @@
+					; Setup package
 (require 'package)
 
+					; No startup package
 (setq package-enable-at-startup nil)
 
+					; Set package sources
 (setq package-archives '(("marmalade" . "http://marmalade-repo.org/packages/")
 			 ("ELPA"  . "http://tromey.com/elpa/")
 			 ("gnu"   . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
 			 ("org" . "https://orgmode.org/elpa/")))
 
+					; Initialize package
 (package-initialize)
 
+					; Bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+					; Emacs variables
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,7 +28,11 @@
  '(custom-enabled-themes (quote (manoj-dark)))
  '(custom-safe-themes
    (quote
-    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))))
+    ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(org-hide-block-startup t)
+ '(package-selected-packages
+   (quote
+    (format-all org htmlize which-key volume use-package try threes switch-window swiper sudo-ext sudo-edit spacemacs-theme spaceline-all-the-icons smex smartparens slime rainbow-mode rainbow-delimiters python pulseaudio-control pretty-mode popup-kill-ring paren-completer paredit-menu org-bullets mark-multiple jedi ido-vertical-mode hungry-delete highlight-parentheses golden-ratio gited exwm expand-region dmenu dashboard csv-mode company-statistics company-math company-ebdb beacon ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,4 +42,5 @@
  '(org-level-1 ((t (:foreground "LightSkyBlue" :height 1.2))))
  '(org-level-2 ((t (:foreground "LightGoldenrod" :height 1.1)))))
 
+					; Load custom config
 (org-babel-load-file (expand-file-name "~/.emacs.d/lzconf.org"))
